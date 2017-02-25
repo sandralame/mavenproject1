58809e0bca7e6645ff8ccdbb79b333236faabf8e
1061723419 <1061723419@S108_PC14.fup.edu>
2017-02-24 21:06:37 -0500

    version inicial de mi proyecto

---
+++ src/main/java/com/edu/ems/mavenproject2/Pelicula.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edu.ems.mavenproject2;
+
+/**
+ *
+ * @author 1061723419
+ */
+public class Pelicula {
+    
+}

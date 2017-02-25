ac074fbfccbc054b7db3c58159a7932aefa8321f
1061723419 <1061723419@S108_PC14.fup.edu>
2017-02-24 21:08:25 -0500

    agrego clase genero

---
+++ src/main/java/com/edu/ems/mavenproject2/Genero.java
@@ -0,0 +1,15 @@
+package com.edu.ems.mavenproject2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1061723419
+ */
+public class Genero {
+    
+}
